ecd95dd3267a0f3d3c0177d2c530e8c43dcb0a01
M Haidar Hanif <git@mhaidarhanif.com>
2023-01-14 10:36:13 +0700

    feat: create a simple salary check program

---
+++ index.js
@@ -0,0 +1,15 @@
+const nationalIdNumber = "238913892830129301212";
+const salaryPerYear = 100_000_000;
+const salaryPerYearInRupiah = `Rp ${salaryPerYear}`;
+
+console.log(`Salary/year = ${salaryPerYearInRupiah}/year`);
+
+if (typeof salaryPerYear === "number") {
+  if (salaryPerYear > 0) {
+    console.log("Congratulations!");
+    console.log(`Because you have a salary of ${salaryPerYearInRupiah}`);
+  } else {
+    console.log("Sorry to hear that.");
+    console.log(`Because your salary is ${salaryPerYearInRupiah}`);
+  }
+}
