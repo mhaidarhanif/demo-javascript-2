7ee29495bdcddf13175b49c5cb0e3f5d41f844b5
M Haidar Hanif <git@mhaidarhanif.com>
2023-01-14 11:38:44 +0700

    feat: modify student to have degree title and age

--- student-profiles/index.js
+++ student-profiles/index.js
@@ -1,16 +1,23 @@
 const dataStudents = [
-  "Abdehi",
-  "Arif",
-  "Bernhard",
-  "Farhan",
-  "Haidar",
-  "Jeffry",
-  "Karim",
-  "Rofiq",
+  { name: "Abdehi" },
+  { name: "Arif" },
+  { name: "Bernhard" },
+  { name: "Farhan" },
+  { name: "Haidar" },
+  { name: "Jeffry" },
+  { name: "Karim" },
+  { name: "Rofiq" },
 ];
 
 for (let index = 0; index < dataStudents.length; index++) {
   const student = dataStudents[index];
+  const newStudent = {
+    name: `${student.name} S.T.`,
+    age: 20 + index,
+  };
+  dataStudents[index] = newStudent;
 
-  console.log(student);
+  console.log(`${newStudent.name} is ${newStudent.age} years old`);
 }
+
+console.log(dataStudents);
